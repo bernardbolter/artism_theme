9d9f73fba5d607f0bb6659ccaf94273065bd025e
Bernard Bolter <bemajor@Bernards-iMac.local>
2017-01-21 13:22:48 -0800

    added head tag information

--- header.php
+++ header.php
@@ -1,10 +1,56 @@
 <!DOCTYPE html>
-<html <?php language_attributes(); ?> class="no-js no-svg">
+<html <?php language_attributes(); ?>>
 <head>
+<meta charset="utf-8">
+<meta http-equiv="x-ua-compatible" content="ie=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
 <meta charset="<?php bloginfo( 'charset' ); ?>" />
-<meta name="viewport" content="width=device-width, initial-scale=1" />
+<title><?php wp_title('&middot;','true','right'); ?><?php bloginfo('name'); ?></title>
+
+<meta name="description" content="The Artwork of Bernard John Bolter IV, this website is the archive and catalog of all his work.">
+<meta name="subject" content="Contemporary American Artwork">
+<meta name="subject" content="Display of Bernard BOtler's Artwork">
+<meta name="topic" content="Art">
+<meta name="summary" content="A Catalog of Bernard Bolter's Artwork">
+<meta name="url" content="https://artwork.bernardbolter.com/">
+<meta name="category" content="Art">
+<meta name="coverage" content="Worldwide">
+<meta name="distribution" content="Global">
+<meta name="rating" content="General">
+<meta name="ICBM" content="37.7396940,-122.4843840">
+<meta name="geo.position" content="37.7396940;-122.4843840">
+<meta name="geo.region" content="US-CA">
+<meta name="geo.placename" content="San Francisco">
+
+<meta name="robots" content="index,follow,noodp">
+<meta name="googlebot" content="index,follow">
+
+<link rel="icon" href="<?php get_template_directory_uri() ?>/favicons/favicon-16.png" sizes="16x16" type="image/png">
+<link rel="icon" href="<?php get_template_directory_uri() ?>/favicon-32.png" sizes="32x32" type="image/png">
+<link rel="icon" href="<?php get_template_directory_uri() ?>/favicon-48.png" sizes="48x48" type="image/png">
+<link rel="icon" href="<?php get_template_directory_uri() ?>/favicon-62.png" sizes="62x62" type="image/png">
+<link rel="icon" href="<?php get_template_directory_uri() ?>/favicon-192.png" sizes="192x192" type="image/png">
+
+<meta property="og:url" content="http://artwork.bernardbolter.com">
+<meta property="og:type" content="website">
+<meta property="og:title" content="Bernard John Bolter the fourth's Artwork">
+<meta property="og:image" content="http://artwork.bernardbolter.com/wp-content/uploads/2007-Venice-Biennale-500x381.jpg">
+<meta property="og:description" content="The Artwork of Bernard John Bolter IV, this website is the archive and catalog of all his work.">
+<meta property="og:site_name" content="Bernard Bolter's Artwork">
+<meta property="og:locale" content="en_US">
+<meta property="article:author" content="Bernard John Bolter IV">
+
+<meta name="twitter:card" content="summary">
+<meta name="twitter:site" content="@bernardbolter">
+<meta name="twitter:creator" content="@bernardbolter">
+<meta name="twitter:url" content="http://artwork.bernardbolter.com">
+<meta name="twitter:title" content="The Artwork of Bernard B0lter">
+<meta name="twitter:description" content="The Artwork of Bernard John Bolter IV, this website is the archive and catalog of all his work.">
+<meta name="twitter:image" content="http://artwork.bernardbolter.com/wp-content/uploads/2007-Venice-Biennale-500x381.jpg">
+
 <link rel="profile" href="http://gmpg.org/xfn/11" />
 <link href="https://fonts.googleapis.com/css?family=Noto+Sans:400,400i,700,700i" rel="stylesheet" />
+
 <?php include 'json-ld.php'; ?>
 
 <?php wp_head(); ?>
